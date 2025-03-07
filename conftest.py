68627e443274c8045cd18fc210085a319bd6fce6
consoledog <console.dog.company@gmail.com>
2025-03-07 13:43:42 +0100

    Extending .gitignore, and adding screenshot on failure

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from utils.driver_factory import get_driver
 import yaml
+import os
+from datetime import datetime
 
 @pytest.fixture(scope="function")
 def driver():
@@ -12,3 +14,19 @@ def driver():
 def config():
     with open("config/config.yaml") as file:
         return yaml.safe_load(file)
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    result = outcome.get_result()
+    if result.when == "call" and result.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            timestamp =  datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            test_name = item.name
+            screenshot_dir = os.path.join(os.getcwd(), "screenshots")
+            os.makedirs(screenshot_dir, exist_ok=True)
+            file_name = f"{test_name}_{timestamp}.png"
+            file_path = os.path.join(screenshot_dir, file_name)
+            driver.save_screenshot(file_path)
+            print(f"\n📸 Screenshot saved: {file_path}")
